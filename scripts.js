583bc506f9bbf290ada193b2ef19b2597c603759
Simeon <simeondimow@gmail.com>
2022-11-23 15:51:56 +0100

    PWA reupload from Gitlab

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//sidebar toggle
+var sidebarOpen = false;
+
+function openSidebar() {
+  var sidebar = document.getElementById("sidebar");
+
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
